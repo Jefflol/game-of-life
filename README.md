3ad68426d77ad3c26c3f490b6bc9042940227230
JefflolC <35282593+JefflolC@users.noreply.github.com>
2019-11-20 15:59:04 -0800

    Updated styling, responsiveness, ux, and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Game of Life
+* Control by selecting cells on the canvas and click the buttons (or spacebar) to run ticks.
+* More information about Game of Life can be found [here].(https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
--- gol.js
+++ gol.js
@@ -10,6 +10,25 @@ const DEAD_VALUE = 0;
 const BLUE_VALUE = 1;
 const RED_VALUE = 2;
 
+// const CELL_BLUE_COLOUR = '#3F7CAC';
+// const CELL_RED_COLOUR = '#F03A47';
+const CELL_BLUE_COLOUR = '#00ADB5';
+const CELL_RED_COLOUR = '#D62246';
+const CELL_BLACK_COLOUR = '#393E46'
+
+var gameBoard = Array.from(Array(GRID_SIZE_X), _ => Array(GRID_SIZE_Y).fill(0));
+var changes = [];
+
+var curr_cell = {
+  X: 0,
+  Y: 0,
+  color: CELL_BLUE_COLOUR,
+  value: BLUE_VALUE
+};
+
+const selectionDisplay = document.getElementById('selectionDisplay');
+selectionDisplay.style.backgroundColor = curr_cell.color;
+
 const run10btn = document.getElementById('run-10');
 const run25btn = document.getElementById('run-25');
 const run50btn = document.getElementById('run-50');
@@ -22,17 +41,6 @@ run50btn.addEventListener("click", runTicks, false);
 
 document.addEventListener("click", getCellPosition);
 document.addEventListener('keydown', keyPressed);
-
-var gameBoard = Array.from(Array(GRID_SIZE_X), _ => Array(GRID_SIZE_Y).fill(0));
-var changes = [];
-
-var curr_cell = {
-  X: 0,
-  Y: 0,
-  color: 'blue',
-  value: BLUE_VALUE
-};
-
 
 function runTicks(e) {
   let i = 0;
@@ -60,7 +68,7 @@ function runTick() {
           X: col,
           Y: row,
           value: DEAD_VALUE,
-          color: 'black'
+          color: CELL_BLACK_COLOUR
         });
       }
 
@@ -90,7 +98,7 @@ function updateBoard() {
 }
 
 function returnCellColor(count) {
-  return count.blueCount > count.redCount ? 'blue' : 'red';
+  return count.blueCount > count.redCount ? CELL_BLUE_COLOUR : CELL_RED_COLOUR;
 }
 
 function returnCellValue(count) {
@@ -127,7 +135,7 @@ function selectCell(cell = curr_cell) {
       drawSquare(cell.X, cell.Y, cell.color, cell.color);
       gameBoard[cell.Y][cell.X] = cell.value;
     } else {  // clear cell
-      drawSquare(cell.X, cell.Y, 'black', 'black');
+      drawSquare(cell.X, cell.Y, CELL_BLACK_COLOUR, CELL_BLACK_COLOUR);
       gameBoard[cell.Y][cell.X] = DEAD_VALUE;
     }
   }
@@ -153,7 +161,7 @@ function getMousePosition(event) {
 
 (function drawCanvas() {
   ctx.beginPath();
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = CELL_BLACK_COLOUR;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.closePath();
 }());
@@ -173,12 +181,24 @@ function keyPressed(event) {
   }
   /* [1] */
   if(event.keyCode == 49) {
-    curr_cell.color = 'blue';
+    curr_cell.color = CELL_BLUE_COLOUR;
     curr_cell.value = BLUE_VALUE;
+    selectionDisplay.style.backgroundColor = CELL_BLUE_COLOUR;
   }
   /* [2] */
   if(event.keyCode == 50) {
-    curr_cell.color = 'red';
+    curr_cell.color = CELL_RED_COLOUR;
     curr_cell.value = RED_VALUE;
+    selectionDisplay.style.backgroundColor = CELL_RED_COLOUR;
   }
 }
+
+(function demoSetup() {
+  changes.push({X: 5, Y: 5, color: CELL_BLUE_COLOUR, value: BLUE_VALUE});
+  changes.push({X: 6, Y: 5, color: CELL_BLUE_COLOUR, value: BLUE_VALUE});
+  changes.push({X: 7, Y: 5, color: CELL_BLUE_COLOUR, value: BLUE_VALUE});
+  changes.push({X: 6, Y: 6, color: CELL_BLUE_COLOUR, value: BLUE_VALUE});
+  changes.push({X: 7, Y: 4, color: CELL_BLUE_COLOUR, value: BLUE_VALUE});
+
+  updateBoard();
+}());
